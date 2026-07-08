d6b38f0655e7dc52f49ac455266cbfa359b6f9ae
kirthicks57 <roshankirthick107@gmail.com>
2026-07-08 09:24:09 +0530

    Add files via upload

---
+++ train.py
@@ -0,0 +1,52 @@
+import joblib
+import pandas as pd
+import seaborn as sns
+import os
+
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, OrdinalEncoder
+from sklearn.linear_model import LogisticRegression
+
+df=sns.load_dataset("titanic")
+df=df[["pclass","sex","age","fare","survived"]]
+
+x=df.drop("survived",axis=1)
+y=df["survived"]
+
+numeric_feature=["age","fare"]
+categorical_feature=["sex","pclass"]
+
+
+numeric_pipeline =Pipeline([
+    ("imputer",SimpleImputer(strategy="median")),
+    ("Scaler",StandardScaler())
+])
+
+
+
+categorical_pipeline=Pipeline([
+    ("imputer",SimpleImputer(strategy="most_frequent")),
+    ("encoder",OrdinalEncoder())
+])
+
+
+preprocessor =ColumnTransformer([
+    ("num",numeric_pipeline, numeric_feature),
+    ("cat",categorical_pipeline, categorical_feature)
+])
+
+
+pipeline=Pipeline([
+    ("preprocessor",preprocessor),
+    ("classifier",LogisticRegression())
+    ])
+
+pipeline.fit(x,y)
+
+os.makedirs("model",exist_ok=True)
+
+joblib.dump(pipeline, "model/pipeline.pkl")
+
+print("Pipeline saved successfully.")
